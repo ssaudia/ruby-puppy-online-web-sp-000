f6e3a88d6077e63edeb95164e55288e6c5b95ca1
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-16 22:14:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all.each do |d|
+      puts d.name
+    end
+  end
+end
